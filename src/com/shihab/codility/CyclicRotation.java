80fbcb338924eda9d3e531b2124c9e13ab4017af
shihab_07 <shihab@sharetrip.net>
2021-09-01 19:09:57 +0600

    Array cyclick rotation to right

--- src/com/shihab/codility/CyclicRotation.java
+++ src/com/shihab/codility/CyclicRotation.java
@@ -4,8 +4,27 @@ public class CyclicRotation {
 
     /**
      * https://www.youtube.com/watch?v=qQWPTHkA9N4&ab_channel=DaveKirkwood
+     * <p>
+     * A = [3,8,9,7,6]  rotate to right K Times
      */
-    public static int solution(int N, int[] A) {
-        return 0;
+    public int[] solution(int[] A, int k) {
+        if (A.length != 0) {
+            for (int i = 0; i < k; i++) {
+                int lastValue = A[A.length - 1];
+                for (int j = A.length - 2; j >= 0; j--) {
+                    A[j + 1] = A[j];
+                }
+                A[0] = lastValue;
+            }
+        }
+        return A;
+    }
+
+    public static void main(String[] args) {
+        CyclicRotation cyclicRotation = new CyclicRotation();
+        int[] result = cyclicRotation.solution(new int[]{3, 8, 9, 7, 6}, 3);
+        for (int i : result) {
+            System.out.print(i + " ");
+        }
     }
 }
