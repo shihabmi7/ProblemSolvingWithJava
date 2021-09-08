9aeea28ffd5f121215f083dabe9ac424f35f02d3
shihab_07 <shihab@sharetrip.net>
2021-09-08 18:41:41 +0600

    Dominator added

---
+++ src/com/shihab/codility/EquiLeader.java
@@ -0,0 +1,15 @@
+package com.shihab.codility;
+
+import java.util.HashMap;
+
+public class EquiLeader {
+
+    public int solution(int[] A) {
+        // need to solve
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new EquiLeader().solution(new int[]{3, 4, 3, 2, 3, -1, 3, 3}));
+    }
+}
