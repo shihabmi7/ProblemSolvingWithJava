4d59445660ed7a6657e8495beea0be15f7892436
shihab_07 <shihab@sharetrip.net>
2021-09-08 16:19:13 +0600

    codlity : braket , fish , nested string

--- src/com/shihab/codility/BraketSolution.java
+++ src/com/shihab/codility/BraketSolution.java
@@ -7,15 +7,41 @@ public class BraketSolution {
     //https://www.youtube.com/watch?v=CGUvXpe4iGM&list=PLTMybUaeagJbTAelBd-pGBuAngpPtnw8b&index=19&ab_channel=DaveKirkwood
     public int solution(String S) {
         Stack<String> stack = new Stack<>();
-
         for (int i = 0; i < S.length(); i++) {
-
+            String ch = S.substring(i, i + 1);
+            if (ch.equals("(") || ch.equals("[") || ch.equals("{")) {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) {
+                    return 0;
+                }
+                if (ch.equals(")")) {
+                    if (!stack.pop().equals("(")) {
+                        return 0;
+                    }
+                }
+                if (ch.equals("]")) {
+                    if (!stack.pop().equals("[")) {
+                        return 0;
+                    }
+                }
+                if (ch.equals("}")) {
+                    if (!stack.pop().equals("{")) {
+                        return 0;
+                    }
+                }
+            }
         }
-        return 0;
+        if (!stack.isEmpty()) {
+            return 0;
+        }
+        return 1;
     }
 
     public static void main(String[] args) {
         BraketSolution number = new BraketSolution();
+        System.out.println(number.solution("{[()()]}"));
         System.out.println(number.solution(""));
+        //System.out.println(number.solution("{[()[()]}"));
     }
 }
