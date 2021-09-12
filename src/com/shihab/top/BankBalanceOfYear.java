6af5ca7efd69ecaca783d3ae4efb2798645ec95a
shihab_07 <shihab@sharetrip.net>
2021-09-12 14:29:34 +0600

    Top: Bank Balance code is updated

--- src/com/shihab/top/BankBalanceOfYear.java
+++ src/com/shihab/top/BankBalanceOfYear.java
@@ -1,6 +1,7 @@
 package com.shihab.top;
 
-public class Bank {
+public class BankBalanceOfYear {
+
     public int solution(int[] A, String[] D) {
         int balance = 0;
         int disCountMonth = 0;
@@ -10,26 +11,24 @@ public class Bank {
             balance += currentAmount;
 
             int bal = 0;
-            int preMonth = getCurrentMonth(D[0]);
+            int preMonth = getCurrentMonth(D[i]);
             if (currentAmount < 0) {
-
                 for (int j = i; j < D.length; j++) {
                     int curMonth = getCurrentMonth(D[j]);
                     currentAmount = A[j];
                     if (currentAmount < 0) {
                         if (curMonth == preMonth) {
                             bal += Math.abs(currentAmount);
-                        }
-
-                        if (bal >= 100) {
-                            disCountMonth++;
+                            if (bal >= 100) {
+                                disCountMonth++;
+                            }
                         }
                     }
                     preMonth = curMonth;
                 }
             }
         }
-        balance = balance - getDiscount(disCountMonth);
+        balance -= getDiscount(disCountMonth);
         return balance;
     }
 
@@ -43,17 +42,17 @@ public class Bank {
     }
 
     public static void main(String[] args) {
-//        System.out.println(new Bank().solution(new int[]{100, 100, 100, -10},
-//                new String[]{"2020-12-31", "2020-12-31", "2020-12-03", "2020-12-29"}));
-
-        System.out.println(new Bank().solution(new int[]{180, -50, -25, -25},
+        System.out.println(new BankBalanceOfYear().solution(new int[]{100, 100, 100, -10},
                 new String[]{"2020-12-31", "2020-12-31", "2020-12-03", "2020-12-29"}));
 
-//        System.out.println(new Bank().solution(new int[]{180, -50, -25, -25},
-//                new String[]{"2020-12-31", "2020-12-31", "2020-12-03", "2020-12-29"}));
+        System.out.println(new BankBalanceOfYear().solution(new int[]{280, -50, -25, -25, 0,},
+                new String[]{"2020-12-31", "2020-12-31", "2020-08-03", "2020-12-29", "2020-07-01"}));
+
+        System.out.println(new BankBalanceOfYear().solution(new int[]{180, -50, -25, -25},
+                new String[]{"2020-12-31", "2020-12-31", "2020-12-03", "2020-12-29"}));
 
         //   System.out.println(getCurrentMonth("2020-12-31"));
-//        System.out.println(new Bank().solution(new int[]{100,100,100,-10},
+//        System.out.println(new BankBalanceOfYear().solution(new int[]{100,100,100,-10},
 //                new String[]{"2020-12-31","2020-12-03","2020-12-29"}));
     }
 }
